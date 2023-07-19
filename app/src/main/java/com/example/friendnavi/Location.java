063b26e9b52a4f0a3a7229a71bbb4d69827a7013
Naki <nhs120417@gmail.com>
2023-07-19 16:55:35 +0900

    - 위치 확인 페이지 초안

---
+++ app/src/main/java/com/example/friendnavi/Location.java
@@ -0,0 +1,88 @@
+package com.example.friendnavi;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.CameraPosition;
+import com.naver.maps.map.MapView;
+import com.naver.maps.map.NaverMap;
+import com.naver.maps.map.OnMapReadyCallback;
+
+public class Location extends AppCompatActivity implements OnMapReadyCallback {
+
+    String TAG = "위치 확인 페이지";
+
+    private MapView mapView;
+    private static NaverMap naverMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+        Log.v(TAG, "onCreate 호출");
+
+        setMapView(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v(TAG, "onStart 호출");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(TAG, "onResume 호출");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(TAG, "onPause 호출");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.v(TAG, "onRestart 호출");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v(TAG, "onStop 호출");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG, "onDestroy 호출");
+    }
+
+    // Custom Method
+
+    public void setMapView(Bundle bundle) {
+        //네이버 지도
+        mapView = (MapView) findViewById(R.id.naverMap);
+        mapView.onCreate(bundle);
+        mapView.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(@NonNull NaverMap naverMap)
+    {
+        this.naverMap = naverMap;
+
+        CameraPosition cameraPosition = new CameraPosition(
+                new LatLng(37.27175, 127.01395),  // 위치 지정
+                9                           // 줌 레벨
+        );
+        naverMap.setCameraPosition(cameraPosition);
+    }
+
+}
