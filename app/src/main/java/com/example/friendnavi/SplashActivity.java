30f9fdc5beedc820a5430d7a6c2c9f3929e66507
Naki <nhs120417@gmail.com>
2023-07-23 15:41:44 +0900

    - 스플래시 화면

---
+++ app/src/main/java/com/example/friendnavi/SplashActivity.java
@@ -0,0 +1,75 @@
+package com.example.friendnavi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+
+public class SplashActivity extends AppCompatActivity {
+
+    String TAG = "스플래쉬";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        moveMain(1);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.v(TAG, "onStart 호출");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.v(TAG, "onResume 호출");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.v(TAG, "onPause 호출");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.v(TAG, "onRestart 호출");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.v(TAG, "onStop 호출");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.v(TAG, "onDestroy 호출");
+    }
+
+    // Custom Method
+
+    private void moveMain(int sec) {
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+
+                startActivity(intent);
+
+                finish();
+            }
+        }, 1000 * sec);
+    }
+
+}
