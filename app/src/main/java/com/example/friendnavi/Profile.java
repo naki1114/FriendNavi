e12eb2d3a6285b01155dd0e1f07b6ea379d9c9f4
Naki <nhs120417@gmail.com>
2023-08-12 18:12:24 +0900

    - 프로필 페이지 로그아웃, 회원탈퇴 버튼 생성 - 회원탈퇴 버튼 클릭 시, 카카오 토큰 제거

--- app/src/main/java/com/example/friendnavi/Profile.java
+++ app/src/main/java/com/example/friendnavi/Profile.java
@@ -1,24 +1,40 @@
 package com.example.friendnavi;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.kakao.sdk.user.UserApiClient;
+
+import kotlin.Unit;
+import kotlin.jvm.functions.Function1;
+
 public class Profile extends Fragment {
 
     String TAG = "F_프로필 페이지";
+
+    View viewActivity;
+
+    Context context;
+
+    Button btnLogout;
+    Button btnWithdraw;
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
         Log.v(TAG, "onAttach()");
+
+        this.context = context;
     }
 
     @Override
@@ -30,7 +46,26 @@ public class Profile extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         Log.v(TAG, "onCreateView()");
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+
+        viewActivity = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        initBtn();
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toLogin();
+            }
+        });
+        btnWithdraw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                kakaoWithdraw();
+                toLogin();
+            }
+        });
+
+        return viewActivity;
     }
 
     @Override
@@ -90,6 +125,29 @@ public class Profile extends Fragment {
     public void onDetach() {
         super.onDetach();
         Log.v(TAG, "onDetach()");
+    }
+
+    // Custom Method
+
+    public void initBtn() {
+        Log.v(TAG, "버튼 참조");
+        btnLogout = viewActivity.findViewById(R.id.btnLogout);
+        btnWithdraw = viewActivity.findViewById(R.id.btnWithdraw);
+    }
+
+    public void toLogin() {
+        Intent toLoginActivity = new Intent(getActivity(), Login.class);
+        startActivity(toLoginActivity);
+        getActivity().finish();
+    }
+
+    public void kakaoWithdraw() {
+        UserApiClient.getInstance().unlink(new Function1<Throwable, Unit>() {
+            @Override
+            public Unit invoke(Throwable throwable) {
+                return null;
+            }
+        });
     }
 
 }
